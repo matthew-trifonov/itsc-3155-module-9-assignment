6c94ba44f4cce0f1bc36455e7700d24d2f40233d
gio uhhh <giochula17@gmail.com>
2023-03-19 14:07:23 -0400

    added two unit assert statements

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -8,7 +8,12 @@ def test_create_movie():
     entered_director_name = "Test director name"
     entered_rating = 3
     test_movie = movie_repository.create_movie(entered_movie_name,entered_director_name,entered_rating)
+    test_movie_id = test_movie.movie_id
+    test_movie_dict = movie_repository.get_all_movies
 
     assert test_movie.title == entered_movie_name
     assert test_movie.director == entered_director_name
     assert test_movie.rating == entered_rating
+    
+    assert movie_repository.get_movie_by_title(entered_movie_name) == test_movie
+    assert movie_repository.get_movie_by_id(test_movie_id) == test_movie 
