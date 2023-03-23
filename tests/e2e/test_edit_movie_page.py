c05fe332ddc4473c8e4d295361f89bb97d6c58f5
Ali-Alzayer <89823176+Ali-Alzayer@users.noreply.github.com>
2023-03-22 23:40:34 -0400

    Final commit

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -1,1 +1,24 @@
 # TODO: Feature 5
+from src.repositories.movie_repository import get_movie_repository
+
+movie_repository = get_movie_repository()
+
+def test_edit_movie(test_app):
+    # Creates test movie and assigns it to test single movie.
+    test_movie = movie_repository.create_movie("Back to the Future", "Robert Zemeckis", 4)
+
+    response = test_app.get(f'/movies/{test_movie.movie_id}/edit')
+    assert response.status_code == 200
+
+    response = test_app.post(f'/movies/{test_movie.movie_id}', data={
+        "name": "test movie name",
+        "director-name": "test director name",
+        "select-rating": 3
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.data.decode('utf-8')
+    assert 'test movie name' in data
+    assert 'test director name' in data
+    
+
+    
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,23 @@
 # TODO: Feature 5
+from src.repositories.movie_repository import get_movie_repository
+movie_repository = get_movie_repository()
+
+def test_update_movie():
+    entered_movie_name = "Test movie name"
+    entered_director_name = "Test director name"
+    entered_rating = 3
+    test_movie = movie_repository.create_movie(entered_movie_name,entered_director_name,entered_rating)
+    test_movie_id = test_movie.movie_id
+
+    new_movie_name = "New movie name"
+    new_director_name = "New movie director"
+    new_rating = 4
+
+    test_movie = movie_repository.update_movie(test_movie_id,new_movie_name,new_director_name,new_rating)
+
+    assert test_movie.title == new_movie_name
+    assert test_movie.director == new_director_name
+    assert test_movie.rating == new_rating
+    
+    assert movie_repository.get_movie_by_title(new_movie_name) == test_movie
+    assert movie_repository.get_movie_by_id(test_movie_id) == test_movie 
