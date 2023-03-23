988247032a4dd3b15c72a6780e2e250d228e159a
Thomas ODonnell <todonnell329@hotmail.com>
2023-03-22 20:48:33 -0400

    Fixed E2E testing and added Unit tests.

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,15 +1,18 @@
 # TODO: Feature 4
-from app import app
 from src.repositories.movie_repository import get_movie_repository
 
 movie_repository = get_movie_repository()
 
 def test_get_single_movie(test_app):
+    # Creates test movie and assigns it to test single movie.
     test_movie = movie_repository.create_movie("Back to the Future", "Robert Zemeckis", 4)
-    test_single_movie = movie_repository.get_movie_by_id(test_movie.movie_id)
+    
 
-    assert test_single_movie.movie_id == test_movie.movie_id
-    assert test_single_movie.title == "Back to the Future"
-    assert test_single_movie.director == "Robert Zemeckis"
-    assert test_single_movie.rating == 4
+    # 
+    response = test_app.get(f'/movies/{test_movie.movie_id}')
+    data = response.data.decode('utf-8')
+    assert response.status_code == 200
+    assert 'Back to the Future' in data
 
+    response = test_app.get(f'/movies/{test_movie.movie_id}/edit')
+    assert response.status_code == 200
